1d6522b8c5119b8fd42aa15cf6d91cb30952e1fe
Balee0 <m.iqbal200303@gmail.com>
2024-02-21 22:08:41 +0700

    menambahkan fungsi, data type & variabel

--- Kotlin-Fundamental/src/main/kotlin/Main.kt
+++ Kotlin-Fundamental/src/main/kotlin/Main.kt
@@ -2,15 +2,34 @@ package org.example
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+// main function dari kotlin
+fun main() {
+    val name = "Iqbal"
+
+    println("hai nama saya $name")
+//    println(name)
+
+// Data tipe & variabel
+    // var identifier: type = initialization
+
+    // var adalah variabel yang dimana bisa di ubah nilai yang sudah di inisiasikan
+    var User: String = "Indonesia"
+    User = "Indonesia Maju"
+    println(User)
+
+    // Berbeda dengan val yang dimana tidak bisa mengubah nilai yang telah di inisiasikan
+    val user: String = "Indonesia"
+//    user = "Idonesia Berkembang" bila dijalankan akan eror
+    println(user)
+
+//    contoh penggunaan variabel
+    val firstWord = "Hallo"
+    val LastWord = "Dunia"
+    println(firstWord + LastWord)
+
+    val valueA: Int = 10
+    val valueB = 20
+    print(valueA + valueB)
+
 }
